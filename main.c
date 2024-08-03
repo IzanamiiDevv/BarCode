93142edb1aaaf48045fe759de36325241894e2de
IzanamiiDevv <rafaeloli596@gmail.com>
2024-08-03 23:47:51 +0800

    Added My Array Library

--- main.c
+++ main.c
@@ -2,26 +2,116 @@
 #include <stdlib.h>
 #include "includes/cli.h"
 
-char* getSourceFile(unsigned short count, char* cmd[]);
+#define CREDIT "IzanamiiDev"
+
+typedef struct {
+    void* list;
+    unsigned int len;
+} Array;
+
+Array array_init() {
+    Array buff;
+    buff.len = 0;
+    buff.list = NULL;
+    return buff;
+}
+
+
+void array_flush(Array* arr) {
+    free(arr->list);
+    arr->len = 0;
+}
+
+#define array_pushBack(type ,arr, value) { \
+    void* new_list; \
+    (arr).len += 1; \
+    new_list = realloc((arr).list, sizeof(type) * (arr).len); \
+    if (new_list == NULL) printf("Error: Memorry Allocation Failed."); \
+    (arr).list = new_list; \
+    ((type*)(arr).list)[(arr).len - 1] = value; \
+};
+
+#define array_popBack(type, arr) { \
+    void* new_list; \
+    (arr).len -= 1; \
+    new_list = realloc((arr).list, sizeof(type) * (arr).len); \
+    if(new_list == NULL) printf("Error: Memorry Allocation Failed."); \
+    (arr).list = new_list; \
+}
+
+#define array_print(format, type, arr) { \
+    printf("["); \
+    for(int i = 0; i < (arr).len; i++) { \
+        printf(format, ((type*)(arr).list)[i]); \
+        if(i == (arr).len - 1) break; \
+        printf(", "); \
+    } \
+    printf("] \n"); \
+}
+
+#define array_take(type, arr, index) ((( type* )(arr).list)[(index)])
+
+
+#define array_forEach(type, arr, callback) { \
+    void(*arrforeach)(type) = callback; \
+    for(int i = 0; i < (arr).len; i++) { \
+        arrforeach((( type* )(arr).list)[i]); \
+    } \
+}
+
+#define array_map(type, arr, callback) { \
+    type (*arrmap)(type) = callback; \
+    for(int i = 0; i < (arr).len; i++) { \
+        (( type* )(arr).list)[i] = arrmap((( type* )(arr).list)[i]); \
+    } \
+}
+
+#define array_mod(type, arr, index, value) (( type* )(arr).list)[index] = value;
+
+#define array_shuffle(type, arr) { \
+    srand(time(0)); \
+    const unsigned int MAX = (arr).len - 1; \
+    const unsigned short MIN = 0; \
+    for(int i = 0; i < (arr).len; i++) {\
+        type temp = (( type* )(arr).list)[i]; \
+        int randn = (rand() % (MAX - MIN + 1)) + MIN; \
+        (( type* )(arr).list)[i] = (( type* )(arr).list)[randn];\
+        (( type* )(arr).list)[randn] = temp; \
+    } \
+}
+
+char* getSource(unsigned short count, char* cmd[]);
 
 int main(unsigned short argc, char* argv[]) {
-    char* data = getSourceFile(argc, argv);
-
-    printf("%s", data);
+    char* data = getSource(argc, argv);
 
     return 0;
 }
 
-char* getSourceFile(unsigned short count, char* cmd[]) {
+char* getSource(unsigned short count, char* cmd[]) {
     char* src = NULL;
 
     for(unsigned short i = 0; i < count; i++) 
         if(strcmp("-s", cmd[i]) == 0) src = cmd[i + 1];
     
-
     if(src == NULL) exit(1);
+
+    FILE *fptr;
+    fptr = fopen(src, "r");
+    if(fptr == NULL) {
+        fclose(fptr);
+        exit(1);
+    };
+
+    char source[100];
+
+    while(fgets(source, 100, fptr)) {
+        printf("%s", source);
+    }
+
+    fclose(fptr);
 
     
 
-    return src;
+    return source;
 }
